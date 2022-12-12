aa21caef4518cdcbb4eaac1bfc3ab3414e0f9851
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-12-12 12:21:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
+  if(word.length <=1) return true
+
+  let start = 0
+  let end = word.length - 1
+
+  while(start <= end){
+    if(word[start] != word[end]){
+      return false
+    }
+    start++
+    end--
+  }
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  1.loop through the string
+  2.check if last element != element at beginning
+  3.if elements do not match return false
+  4.else return true
+  5.increment start
+  6.decrement end
 */
 
 /*
